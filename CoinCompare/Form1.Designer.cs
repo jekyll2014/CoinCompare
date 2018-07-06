45f7d225c4484e47e19d5ac050fc3dae422c5254
Andrey <kalugin@outlook.com>
2018-07-06 18:07:38 +0300

    upd

--- CoinCompare/Form1.Designer.cs
+++ CoinCompare/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button_openCsv
             // 
-            this.button_openCsv.Location = new System.Drawing.Point(13, 13);
+            this.button_openCsv.Location = new System.Drawing.Point(12, 12);
             this.button_openCsv.Name = "button_openCsv";
             this.button_openCsv.Size = new System.Drawing.Size(75, 23);
             this.button_openCsv.TabIndex = 0;
@@ -56,9 +56,9 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 41);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(659, 409);
+            this.dataGridView1.Size = new System.Drawing.Size(660, 409);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
@@ -69,7 +69,7 @@
             // 
             // button_compare
             // 
-            this.button_compare.Location = new System.Drawing.Point(94, 12);
+            this.button_compare.Location = new System.Drawing.Point(93, 12);
             this.button_compare.Name = "button_compare";
             this.button_compare.Size = new System.Drawing.Size(75, 23);
             this.button_compare.TabIndex = 0;
@@ -79,7 +79,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(175, 12);
+            this.button1.Location = new System.Drawing.Point(174, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
